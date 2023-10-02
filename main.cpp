af3fe8ba502424ddb01e71bb1dbfe198c6b8766e
Carles Gradoli Montagud <cgramon@etsinf.upv.es>
2023-10-02 11:42:39 +0200

    Añadido fichero main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "BrazoRobotico.h"
+
+int main(){
+	BrazoRobotico b(0, 0, 0, 0);
+	cout << "Posicion: (" << b.obtenerX() << ", " << b.obtenerY() << ", " <<
+	       	b.obtenerZ() << ")\n";
+	if(b.obtenerObjsuj() == 0)
+		cout << "Objeto no cogido\n";
+	else
+		cout << "Objeto cogido\n";
+
+	b.mover(1, 3, 2);
+
+	cout << "Posicion: (" << b.obtenerX() << ", " << b.obtenerY() << ", " <<
+                b.obtenerZ() << ")\n";
+
+	b.coger();
+
+	if(b.obtenerObjsuj() == 0)
+                cout << "Objeto no cogido\n";
+        else
+                cout << "Objeto cogido\n";
+
+	return 0;
+}
